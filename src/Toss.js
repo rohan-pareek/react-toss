b23f9365d6d205eb76a9cfda9fdcc76294cf8fb0
Rohan Pareek <rohaniscoding@gmail.com>
2020-12-07 23:02:38 +0530

    Initializing project

---
+++ src/Toss.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function Toss() {
+
+    let initailRandomValue = false;
+    let randomValue = 0;
+
+    const doToss = () => {
+        randomValue = Math.floor(Math.random() * 10 + 10);
+        setTimeout(animateResult, 80);
+    }
+
+    const animateResult = () => {
+        let result = initailRandomValue? 'tail': 'head';
+        initailRandomValue = !initailRandomValue;
+        if(randomValue>0) {
+            randomValue--;
+            document.getElementById('toss-section').innerText = result;
+            setTimeout(animateResult, 80);
+        }
+    }
+
+    const reset = () => {
+        document.getElementById('toss-section').innerText = "";
+    }
+
+    return (
+        <div className = "app">
+            <div className = "app-title">
+                <h1>SukhaYaGilla</h1>
+                <p>Gudiyari Cricket Association</p>
+            </div>
+            <div className = "toss-section" id = "toss-section"></div>
+            <button onClick = {doToss}>Toss</button>
+            <button onClick = {reset}>Reset</button>
+            <h3>Made by Rohan</h3>
+        </div>
+    )
+}
