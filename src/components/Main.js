e61019fbfc9c54e5b36d172135f7d79a1c82b0f8
Rohan Pareek <rohaniscoding@gmail.com>
2020-12-14 01:15:31 +0530

    added loader

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,27 +8,29 @@ import Dashboard from './Dashboard';
 import { connect } from 'react-redux';
 import { login } from '../actions/group';
 import SignupForm from './SignupForm';
+import Loader from './Loader';
 
-function Main({login, ...props}) {
+function Main({ login, ...props }) {
 
     useEffect(() => {
-        if(localStorage.getItem('groupID')) {
+        if (localStorage.getItem('groupID')) {
             login(JSON.stringify({
                 groupID: localStorage.getItem('groupID')
             }))
         }
-        
+
     }, [login])
 
     return (
         <div className="app">
+            {props.isLoading && <Loader />}
             <Router>
                 <Header />
                 <Switch>
                     <Route exact path="/" >
                         {props.isLoggedIn ? <Redirect to="/dashboard" /> : <><Toss />
-                        <LoginForm />
-                        <SignupForm />
+                            <LoginForm />
+                            <SignupForm />
                         </>}
                     </Route>
                     <Route path="/dashboard" component={Dashboard} />
@@ -42,6 +44,7 @@ function Main({login, ...props}) {
 const mapStateToProps = (state) => ({
     groupID: state.group.group,
     isLoggedIn: state.group.isLoggedIn,
+    isLoading: state.group.loader
 })
 
 export default connect(mapStateToProps, { login })(Main);
--- src/reducers/group.js
+++ src/reducers/group.js
@@ -3,17 +3,17 @@ const { LOGIN, LOGOUT, LOADER, SUCCESS, ERROR } = require("../actionTypes");
 export const groupReducer = (state = { groupID: null, isLoggedIn: false, loader: false, successMessage: null, errorMessage: null }, action) => {
     switch (action.type) {
         case LOGIN:
-            return { ...state, groupID: action.payload, isLoggedIn: true, errorMessage: null, successMessage: null };
+            return { ...state, groupID: action.payload, isLoggedIn: true, errorMessage: null, successMessage: null, loader: false };
         case LOGOUT:
-            return { ...state, groupID: null, isLoggedIn: false, errorMessage: null, successMessage: null };
+            return { ...state, groupID: null, isLoggedIn: false, errorMessage: null, successMessage: null, loader: false };
         case LOADER: {
-            return { ...state, loader: action.payload }
+            return { ...state, loader: action.active }
         }
         case ERROR: {
-            return { ...state, errorMessage: action.payload, successMessage: null }
+            return { ...state, errorMessage: action.payload, successMessage: null, loader: false }
         }
         case SUCCESS: {
-            return { ...state, errorMessage: null, successMessage: action.payload }
+            return { ...state, errorMessage: null, successMessage: action.payload, loader: false }
         }
         default:
             return state;
